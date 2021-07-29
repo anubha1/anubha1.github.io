277fafffc2abe544ff5eeb0a314f5992bde85793
anubha1 <anubhak2011@gmail.com>
2021-07-29 15:50:56 -0700

    added breakfast cereal comparison project

---
+++ cereals-analysis/script.js
@@ -0,0 +1,500 @@
+data = [
+    {
+      "Name": "Honey Bunches of Oats with Almonds",
+      "Link": "https://www.honeybunchesofoats.com/products/with-almonds/",
+      "Serving": 42,
+      "Calories": 170,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 8,
+      "Fiber": 9,
+      "AddSug": 15,
+      "VitD": 10,
+      "Calcium": 0,
+      "Iron": 90,
+      "Potassium": 0
+    },
+    {
+      "Name": "Kellogg's Corn Flakes",
+      "Link": "https://www.kelloggs.com/en_US/products/kellogg-s-corn-flakes-cereal-product.html",
+      "Serving": 42,
+      "Calories": 150,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 13,
+      "Fiber": 5,
+      "AddSug": 8,
+      "VitD": 15,
+      "Calcium": 0,
+      "Iron": 60,
+      "Potassium": 0
+    },
+    {
+      "Name": "Original Cheerios",
+      "Link": "https://www.cheerios.com/products/original-cheerios/",
+      "Serving": 39,
+      "Calories": 140,
+      "SatFat": 3,
+      "Cholesterol": 0,
+      "Sodium": 8,
+      "Fiber": 14,
+      "AddSug": 4,
+      "VitD": 10,
+      "Calcium": 10,
+      "Iron": 70,
+      "Potassium": 6
+    },
+    {
+      "Name": "Honey Nut Cheerios",
+      "Link": "https://www.cheerios.com/products/honey-nut-cheerios/",
+      "Serving": 37,
+      "Calories": 140,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 9,
+      "Fiber": 10,
+      "AddSug": 24,
+      "VitD": 10,
+      "Calcium": 10,
+      "Iron": 20,
+      "Potassium": 4
+    },
+    {
+      "Name": "Kellogg's Frosted Flakes",
+      "Link": "https://www.kelloggs.com/en_US/products/kellogg-s-frosted-flakes-cereal-product.html",
+      "Serving": 37,
+      "Calories": 130,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 8,
+      "Fiber": 4,
+      "AddSug": 24,
+      "VitD": 10,
+      "Calcium": 0,
+      "Iron": 40,
+      "Potassium": 0
+    },
+    {
+      "Name": "Cinnamon Toast Crunch",
+      "Link": "https://www.cinnamontoastcrunch.com/product/cinnamon-toast-crunch/",
+      "Serving": 41,
+      "Calories": 170,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 10,
+      "Fiber": 7,
+      "AddSug": 24,
+      "VitD": 10,
+      "Calcium": 10,
+      "Iron": 20,
+      "Potassium": 0
+    },
+    {
+      "Name": "Original Lucky Charms",
+      "Link": "https://www.luckycharms.com/products/original-lucky-charms/",
+      "Serving": 36,
+      "Calories": 140,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 10,
+      "Fiber": 7,
+      "AddSug": 24,
+      "VitD": 10,
+      "Calcium": 10,
+      "Iron": 20,
+      "Potassium": 0
+    },
+    {
+      "Name": "Kellogg's Froot Loops",
+      "Link": "https://www.kelloggs.com/en_US/products/froot-loops-cereal.html",
+      "Serving": 39,
+      "Calories": 150,
+      "SatFat": 3,
+      "Cholesterol": 0,
+      "Sodium": 9,
+      "Fiber": 8,
+      "AddSug": 24,
+      "VitD": 10,
+      "Calcium": 0,
+      "Iron": 25,
+      "Potassium": 0
+    },
+    {
+      "Name": "Kellogg's Special K Original",
+      "Link": "https://www.specialk.com/en_US/products/cereal/original-cereal.html",
+      "Serving": 39,
+      "Calories": 150,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 12,
+      "Fiber": 2,
+      "AddSug": 8,
+      "VitD": 10,
+      "Calcium": 0,
+      "Iron": 60,
+      "Potassium": 0
+    },
+    {
+      "Name": "Kellogg's Raisin Bran Original",
+      "Link": "https://www.kelloggs.com/en_US/products/kellogg-s-raisin-bran-cereal-product.html",
+      "Serving": 59,
+      "Calories": 190,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 9,
+      "Fiber": 26,
+      "AddSug": 18,
+      "VitD": 0,
+      "Calcium": 0,
+      "Iron": 10,
+      "Potassium": 6
+    },
+    {
+      "Name": "Reese's Puffs",
+      "Link": "https://www.generalmillscf.com/products/category/cereal/bulk/reeses-puffs",
+      "Serving": 39,
+      "Calories": 160,
+      "SatFat": 4,
+      "Cholesterol": 0,
+      "Sodium": 9,
+      "Fiber": 7,
+      "AddSug": 24,
+      "VitD": 10,
+      "Calcium": 10,
+      "Iron": 20,
+      "Potassium": 2
+    },
+    {
+      "Name": "Kellogg's Frosted Mini-Wheats",
+      "Link": "https://www.frostedminiwheats.com/en_US/products/kellogg-s-frosted-mini-wheats-bite-size-cereal-product.html",
+      "Serving": 60,
+      "Calories": 210,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 0,
+      "Fiber": 21,
+      "AddSug": 24,
+      "VitD": 0,
+      "Calcium": 0,
+      "Iron": 100,
+      "Potassium": 2
+    },
+    {
+      "Name": "Kellogg's Apple Jacks Cereal",
+      "Link": "https://www.kelloggs.com/en_US/products/kellogg-s-apple-jacks-cereal-product.html",
+      "Serving": 39,
+      "Calories": 150,
+      "SatFat": 3,
+      "Cholesterol": 0,
+      "Sodium": 9,
+      "Fiber": 8,
+      "AddSug": 26,
+      "VitD": 10,
+      "Calcium": 0,
+      "Iron": 25,
+      "Potassium": 0
+    },
+    {
+      "Name": "Quaker Life Cereal Original",
+      "Link": "https://www.quakeroats.com/products/cold-cereals/life-cereal/regular",
+      "Serving": 42,
+      "Calories": 160,
+      "SatFat": 0,
+      "Cholesterol": 0,
+      "Sodium": 9,
+      "Fiber": 10,
+      "AddSug": 16,
+      "VitD": 0,
+      "Calcium": 10,
+      "Iron": 70,
+      "Potassium": 2
+    },
+    {
+      "Name": "Quaker Oats Old Fashioned",
+      "Link": "https://www.quakeroats.com/products/hot-cereals/old-fashioned-oats",
+      "Serving": 40,
+      "Calories": 150,
+      "SatFat": 3,
+      "Cholesterol": 0,
+      "Sodium": 0,
+      "Fiber": 13,
+      "AddSug": 0,
+      "VitD": 0,
+      "Calcium": 0,
+      "Iron": 8,
+      "Potassium": 2
+    },
+    {
+      "Name": "Post Great Grains Crunchy Pecan Cereal",
+      "Link": "https://www.postconsumerbrands.com/great-grains/",
+      "Serving": 55,
+      "Calories": 210,
+      "SatFat": 3,
+      "Cholesterol": 0,
+      "Sodium": 7,
+      "Fiber": 19,
+      "AddSug": 9,
+      "VitD": 10,
+      "Calcium": 0,
+      "Iron": 90,
+      "Potassium": 4
+    }
+];
+
+data.sort((a,b) => ((a.Name < b.Name)? -1 : 1));
+
+// for unwanted: 5% or less DV is considered good
+// for wanted: 20% or more DV is considered good
+const lowDV = 5;
+const highDV = 20;
+const recommended = {
+  "SatFat": 0,
+  "Cholesterol": 0,
+  "Sodium": 0,
+  "Fiber": 1,
+  "AddSug": 0,
+  "VitD": 1,
+  "Calcium": 1,
+  "Iron": 1,
+  "Potassium": 1
+}
+
+// colors
+const brightGreen = "#00cc03";
+const brightRed = "#ff0f00";
+const lightGreen = "#ccffac";
+const lightRed = "#ffbfbb";
+const brightYellow = "#FFDE00";
+const lightYellow = "#FFFCC9";
+
+const svg1 = d3.select('#svg1');
+const field = document.getElementById("nutrition");
+const recommnedationText = document.getElementById("recommendation-text");
+var currField = "SatFat"; 
+var currFieldRecommendation = recommended[currField];
+
+textGroup = svg1.append("g")
+.attr("transform", "translate(10, 10)");
+
+textGroup.append("text")
+.text("Cereal")
+.attr("x", 0)
+.attr("y", 20);
+
+textGroup.append("text")
+.text("% Daily Value")
+.attr("x", 310)
+.attr("y", 20);
+
+barGroup = svg1.selectAll("#bar-group") // group
+.data(data)
+.enter()
+.append("g")
+.attr("transform", function(d, i) {
+    return "translate(10," + (40 + i*30) + ")";
+})
+.attr("id", "#bar-group");
+
+barGroup.append("rect")
+.attr("x", 0)
+.attr("y", 0)
+.attr("width", function(d, i) {
+    return 100 * 3;
+})
+.attr("height", 24)
+.attr("id", "background-rect")
+
+barGroup.append("rect")
+.attr("x", 0)
+.attr("y", 0)
+.attr("width", function(d, i) {
+    return d[currField] * 3;
+})
+.attr("height", 24)
+.attr("id", "dynamic-rect");
+
+barGroup.append("text")
+.text(function(d, i) {
+    return d.Name;
+})
+.attr("x", 2)
+.attr("y", 20)
+.style("font-size", "16px")
+.style("fill", "black");
+
+barGroup.append("text")
+.text(function (d, i) {
+    return d.SatFat + "%";
+})
+.attr("x", 310)
+.attr("y", 20)
+.style("font-size", "16px")
+.attr("id", "dynamic-value");
+
+function update() {
+    currField = field.value;
+    currFieldRecommendation = recommended[currField];
+
+    barGroup.selectAll("#dynamic-rect") 
+    .attr("width", function(d, i) { return d[currField] * 3; })
+    .style("fill", function(d, i) {
+        switch (followsRecommendation(d[currField], currFieldRecommendation)) {
+          case 1:
+            return brightGreen;
+          case 2: 
+            return brightYellow;
+          case 3:
+            return brightRed;
+       }
+    });
+    barGroup.selectAll("#background-rect")
+    .style("fill", function(d, i) {
+        switch (followsRecommendation(d[currField], currFieldRecommendation)) {
+            case 1:
+              return lightGreen;
+            case 2: 
+              return lightYellow;
+            case 3:
+              return lightRed;
+        }
+        return lightRed;
+    });
+
+    barGroup.selectAll("#dynamic-value") 
+    .text(function (d, i) {
+        return d[currField] + "%";
+    })
+
+    recommnedationText.innerText = currFieldRecommendation? (field.options[field.selectedIndex].innerText + ": high intake is recommended.") : 
+    (field.options[field.selectedIndex].innerText + ": low intake is recommended.");
+}
+
+update();
+
+function followsRecommendation(value, wanted) {
+    if(wanted) {
+        if(value >= highDV) {
+            return 1;
+        }
+        else if(value+5 >= highDV) {
+            return 2;
+        }
+        else { 
+            return 3;
+        }
+    }
+    else {
+        if(value <= lowDV) {
+            return 1;
+        }
+        else if(value-5 <= lowDV) {
+            return 2;
+        }
+        else { 
+            return 3;
+        }
+    }
+}
+
+function alphaOrderComparator(first, second) {
+  return (first.Name < second.Name)? -1 : 1;
+}
+
+function cerealComparator(first, second) {
+  return (first[currField] < second[currField])? -1 : 1;
+}
+
+// create color key 
+keyGroup = svg1.append("g")
+.attr("transform", "translate(500, 10)");
+
+keyGroup.append("text")
+.text("Key")
+.attr("y", 20);
+
+keyGroup.append("rect")
+.attr("y", 30)
+.attr("height", 24)
+.attr("width", 24)
+.style("fill", lightGreen);
+
+keyGroup.append("rect")
+.attr("y", 60)
+.attr("height", 24)
+.attr("width", 24)
+.style("fill", lightYellow);
+
+keyGroup.append("rect")
+.attr("y", 90)
+.attr("height", 24)
+.attr("width", 24)
+.style("fill", lightRed);
+
+keyGroup.append("text")
+.attr("y", 50)
+.attr("x", 26)
+.text("Follows recommendation");
+
+keyGroup.append("text")
+.attr("y", 80)
+.attr("x", 26)
+.text("Near recommendation");
+
+keyGroup.append("text")
+.attr("y", 110)
+.attr("x", 26)
+.text("Far from recommendation");
+
+// create plain table
+const tableCols = ["Name", "Serving", "Calories",	"SatFat",	"Cholesterol", "Sodium", "Fiber", "AddSug", "VitD", "Calcium", "Iron", "Potassium"]
+const tableBody = document.getElementById('tableData');
+
+updateTable(data);
+function updateTable(dataArray) {
+  tableBody.innerHTML = "";
+  for(let i = 0; i < dataArray.length; i++) {
+    var tableRow = document.createElement("tr");
+    for(let j = 0; j < tableCols.length; j++) {
+      tableRow.innerHTML += "<td>" + dataArray[i][tableCols[j]] + "</td>";
+    }
+    tableBody.appendChild(tableRow);
+  }
+}
+
+var sortDirections = {
+  Name: false,
+  Serving: true,
+  Calories: true,
+  SatFat: true,
+  Cholesterol: true,
+  Sodium: true,
+  Fiber: true,
+  AddSug: true,
+  VitD: true,
+  Calcium: true,
+  Iron: true,
+  Potassium: true
+}
+
+function sortColumn(columnName) {
+  if(sortDirections[columnName]) {
+    data = data.sort((a,b) => (a[columnName] < b[columnName]? -1 : 1));
+  }
+  else {
+    data = data.sort((a,b) => (a[columnName] > b[columnName]? -1 : 1));
+  }
+  updateTable(data);
+  sortDirections[columnName] = !sortDirections[columnName];
+}
+
+
+// write references 
+var refs = document.getElementById("references");
+for(let i = 0; i < data.length; i++) {
+  var linkURL = data[i].Link;
+  var linkElement = document.createElement("a");
+  linkElement.setAttribute("href", linkURL);
+  linkElement.innerText = linkURL;
+  refs.appendChild(linkElement);
+  refs.innerHTML += "<br>";
+}
